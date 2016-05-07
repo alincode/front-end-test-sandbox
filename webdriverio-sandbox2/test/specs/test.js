f8ee9d7ae9a124f9fb5c97ab69e76182f9077ba3
alincode <alin.code@gmail.com>
2016-05-07 23:03:17 +0800

    update example

--- webdriverio-sandbox2/test/specs/test.js
+++ webdriverio-sandbox2/test/specs/test.js
@@ -1,8 +1,8 @@
 var chai = require('chai');
 global.should = chai.should();
 
-describe('DuckDuckGo search', function() {
-    it('searches for WebdriverIO', function() {
+describe('mokayo', function() {
+    it('check title', function() {
         browser.url('http://blog.mokayo.com/');
         var title = browser.getTitle();
         title.should.be.equal('教你所想學的，用眼樂讀 - blog.mokayo.com'); 
