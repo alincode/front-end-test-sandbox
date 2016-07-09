b3dcff12cb6d94465e8ea60174bad280fbb2b328
alincode <alin.code@gmail.com>
2016-07-09 17:56:15 +0800

    add no angularjs website

--- protractor-sandbox/test/example.spec.js
+++ protractor-sandbox/test/example.spec.js
@@ -3,11 +3,20 @@ var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 var expect = chai.expect;
 
-describe('angularjs homepage', function() {
+describe('angularjs website', function() {
   it('should have learn link.', function(done) {
     browser.get('http://www.angularjs.org');
     var myElement = element(by.css('.learn-link'))
-    expect(myElement.getText()).to.eventually.equal('Learn Angular in your browser for free!');
+    expect(myElement.getText()).to.eventually.equal(
+      'Learn Angular in your browser for free!');
+    done();
+  });
+
+  it('test no angularjs website', function(done) {
+    browser.driver.get('http://webdriver.io/');
+    var myElement = browser.driver.findElement(by.css('h2'));
+    expect(myElement.getText()).to.eventually.equal(
+      'Selenium 2.0 bindings for NodeJS');
     done();
   });
 });
